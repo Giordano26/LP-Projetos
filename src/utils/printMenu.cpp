78cb891fd57674c3f41148606291d7593e0aa92f
MelloTonio <antoniomelloaviao@gmail.com>
2020-08-25 01:17:17 -0300

    feat: add menu interface

---
+++ src/utils/printMenu.cpp
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <time.h>
+
+int printMenu()
+{
+    printf(
+        " _____________________________________ \n"
+        "|                 MENU                |\n"
+        "|                                     |\n"
+        "|          Select an option:          |\n"
+        "|                                     |\n"
+        "|                                     |\n"
+        "|  1) Show Pieces                     |\n"
+        "|                                     |\n"
+        "|  2) ?????????????                   |\n"
+        "|                                     |\n"
+        "|  3) ?????????                       |\n"
+        "|                                     |\n"
+        "|  4) ?????????                       |\n"
+        "|                                     |\n"
+        "|  5) ?????                           |\n"
+        "|                                     |\n"
+        "|  6) Exit                            |\n"
+        "|                                     |\n"
+        "|_____________________________________|\n");
+}
